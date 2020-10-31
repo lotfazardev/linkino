807e58e570c4f526acf5ded8a0c6ce85fdfa4919
Mohammad H. Lotfazar <lotfazardev@users.noreply.github.com>
2020-10-31 09:42:58 +0330

    ADD GraphQL instance into project

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/main.scss';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/main.scss";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloProvider } from "react-apollo";
+import client from "./graphql/instance";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
