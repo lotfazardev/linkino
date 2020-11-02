b406a55c359b4467dba46e7e3f5d436f9a6727a0
Mohammad H. Lotfazar <lotfazardev@users.noreply.github.com>
2020-11-03 01:23:45 +0330

    ADD AddPage ui component instead of pure PostForm

---
+++ src/components/Polymers/AddPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PostForm from '../molecules/PostForm';
+
+const AddPage = () => {
+    return (
+        <div>
+            <PostForm />
+        </div>
+    );
+}
+
+export default AddPage;
